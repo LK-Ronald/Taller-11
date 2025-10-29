8dd03d502922d4027b2a811fb832184cbc43ef57
LkRonald <rpereirac@unicartagena.co>
2025-10-29 17:29:20 -0500

    feat: se agrego el metodo mostrarArea en la clase Figura

--- Taller11/src/main/java/Figura.java
+++ Taller11/src/main/java/Figura.java
@@ -2,4 +2,7 @@ public abstract class Figura {
 
     public abstract double calcularArea();
 
+    public void mostrarArea(){
+        System.out.println("El area de la figura es: " + calcularArea());
+    }
 }
