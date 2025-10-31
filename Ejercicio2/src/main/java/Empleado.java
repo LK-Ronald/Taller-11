94dc321f635e155bb9378297c582659678d44cee
LkRonald <rpereirac@unicartagena.co>
2025-10-31 17:12:54 -0500

    feat: se agrego el metodo mostrarDetalles

--- Ejercicio2/src/main/java/Empleado.java
+++ Ejercicio2/src/main/java/Empleado.java
@@ -1,4 +1,8 @@
 public abstract class Empleado {
 
     public abstract double calcularSalario();
+
+    public void mostrarDetalles(){
+        System.out.println("Salario: "+ calcularSalario());
+    }
 }
